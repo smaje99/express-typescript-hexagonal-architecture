2d3b1fb0bc8f6e8d5a185b64b8f51a809d8ddc58
Sergio Majé <smajefranco@gmail.com>
2023-06-04 22:33:42 -0500

    chore: :wrench: Fix indent for switch case

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,23 +5,20 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   rules: {
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "max-len": ["error", 90],
-    "quotes": [2, "single", { "avoidEscape": true }],
-    "indent": ["error", 2],
-    "prettier/prettier": ["error", { "endOfLine": "auto" }],
-    "curly": "error"
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'max-len': ['error', 90],
+    quotes: [2, 'single', { avoidEscape: true }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    curly: 'error',
   },
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js', "commitlint.config.js", "jest.config.js"],
+  ignorePatterns: ['.eslintrc.js', 'commitlint.config.js', 'jest.config.js'],
 };
